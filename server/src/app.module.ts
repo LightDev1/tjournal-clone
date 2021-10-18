dc1c69f57e07abae72abb0ac03b8aede3ad3da33
LightDev1 <karumasanmy@gmail.com>
2021-10-18 19:30:08 +0300

    Инициировал backend с помощью NestJS, настроил подкючение к базе данных PostgreSql, сделал контроллеры для user, и сделал сущность user

---
+++ server/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserEntity } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '99salavat99',
+      database: 'tjournal',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
